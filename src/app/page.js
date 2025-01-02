15582505b3888790faab1504498ca2e73ab01aae
muhammadkashif20 <kashif28@gmail.com>
2025-01-02 09:52:56 +0500

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div className="my-10">
+    <h1 className="text-center font-semibold text-3xl">HELLO NEXT AUTH CRASH COURSE GOOGLE & GITHUB LOGIN.😍</h1>
+    </div>
+  )
+}
+
+export default page
