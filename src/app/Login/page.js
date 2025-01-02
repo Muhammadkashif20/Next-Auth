d7112bd80df2c6017238614e7fdf836bb5cc1b8a
muhammadkashif20 <kashif28@gmail.com>
2025-01-02 11:19:51 +0500

    Working Complete Google Auth with Next-Auth

--- src/app/Login/page.js
+++ src/app/Login/page.js
@@ -1,20 +1,38 @@
-import { auth, signIn } from "@/app/auth";
+import { auth, signIn, signOut } from "@/app/auth";
+import Image from "next/image";
 import React from "react";
 
 const Login = async () => {
   const session = await auth();
-  console.log("session=>",session);
-  
+  console.log("session=>", session);
+
   return (
-    <div className="container mx-auto flex justify-center items-center">
-      <form
-        action={async () => {
-          "use server";
-          await signIn("google");
-        }}
-      >
-        <button type="submit">Signin with Google</button>
-      </form>
+    <div className="container my-36 flex justify-center items-center">
+      {session ? (
+        <div className="bg-white outline outline-gray-200 w-1/3 h-1/2 flex flex-col items-center justify-center py-4">
+          <Image className="rounded-full p-4 " width={150} height={100} src={session?.user?.image}/>
+          <h1 className="font-semibold text-2xl">{session?.user?.name}</h1>
+          <h2 className="font-normal ">{session?.user?.email}</h2>
+          <form
+          action={async () => {
+            "use server";
+            await signOut("google");
+          }}
+        >
+          <button type="submit">SignOut</button>
+        </form>
+        </div>
+        
+      ) : (
+        <form
+          action={async () => {
+            "use server";
+            await signIn("google");
+          }}
+        >
+          <button type="submit">Signin with Google</button>
+        </form>
+      )}
     </div>
   );
 };
